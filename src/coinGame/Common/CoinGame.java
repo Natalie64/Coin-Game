a5e668e81da3a292df7790e151eaf2ccfbede2ec
Natalie Williams <willin51@ferris.edu>
2020-05-30 16:03:47 -0400

    Added a game loop until the player wishes to leave

--- src/coinGame/Common/CoinGame.java
+++ src/coinGame/Common/CoinGame.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 import java.util.Random;
 /**
  * Basic coin-toss game.
- * @author zorab
- * @version 1.0
+ * @author Natalie Williams
+ * @version 1.1
  */
 public class CoinGame {
 
@@ -14,29 +14,35 @@ public class CoinGame {
 		Random randGen = new Random();
 		int coinFace;	// Which face is facing up. 0 = Heads 1 = Tails
 		String coinChoice;	// Tracks if the user chose heads or tails.
+		String nextRound = "Y";	// Whether or not the player wishes to continue.
 		
-		System.out.print("Heads or tails? H = heads T = Tails\n>");
-		coinChoice = scnr.next();
-		
-		coinFace = randGen.nextInt(2);
-		switch (coinFace){
-			case 0:
-				if (coinChoice.equals("H")) {
-					System.out.println("You win!");
-				}
-				else {
-					System.out.println("You lose...");
-				}
-				break;
+		while (nextRound.equals("Y")) {	// Loops the game until the player chooses to stop playing.
+			System.out.print("Heads or tails? H = heads T = Tails\n>");
+			coinChoice = scnr.next();
 			
-			case 1:
-				if (coinChoice.equals("T")) {
-					System.out.println("You win!");
-				}
-				else {
-					System.out.println("You lose...");
-				}
-				break;
+			coinFace = randGen.nextInt(2);
+			switch (coinFace){
+				case 0:
+					if (coinChoice.equals("H")) {
+						System.out.println("You win!");
+					}
+					else {
+						System.out.println("You lose...");
+					}
+					break;
+				
+				case 1:
+					if (coinChoice.equals("T")) {
+						System.out.println("You win!");
+					}
+					else {
+						System.out.println("You lose...");
+					}
+					break;
+			}
+			
+			System.out.print("Play again? Y = Yes N = No\n>");
+			nextRound = scnr.next();
 		}
 
 	}
