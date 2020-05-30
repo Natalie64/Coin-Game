e07710e3012f6c8b7bd6657c24658b223c3b7a8e
Natalie Williams <willin51@ferris.edu>
2020-05-30 16:18:24 -0400

    Added win-loss count

--- src/coinGame/Common/CoinGame.java
+++ src/coinGame/Common/CoinGame.java
@@ -3,9 +3,9 @@ package coinGame.Common;
 import java.util.Scanner;
 import java.util.Random;
 /**
- * Basic coin-toss game.
+ * Basic coin-toss game with win-loss count.
  * @author Natalie Williams
- * @version 1.1
+ * @version 1.2
  */
 public class CoinGame {
 
@@ -13,6 +13,8 @@ public class CoinGame {
 		Scanner scnr = new Scanner(System.in);
 		Random randGen = new Random();
 		int coinFace;	// Which face is facing up. 0 = Heads 1 = Tails
+		int wins = 0;	// Number of times the player won.
+		int losses = 0;	// Number of times the player lost.
 		String coinChoice;	// Tracks if the user chose heads or tails.
 		String nextRound = "Y";	// Whether or not the player wishes to continue.
 		
@@ -25,18 +27,22 @@ public class CoinGame {
 				case 0:
 					if (coinChoice.equals("H")) {
 						System.out.println("You win!");
+						++wins;
 					}
 					else {
 						System.out.println("You lose...");
+						++losses;
 					}
 					break;
 				
 				case 1:
 					if (coinChoice.equals("T")) {
 						System.out.println("You win!");
+						++wins;
 					}
 					else {
 						System.out.println("You lose...");
+						++losses;
 					}
 					break;
 			}
@@ -44,6 +50,8 @@ public class CoinGame {
 			System.out.print("Play again? Y = Yes N = No\n>");
 			nextRound = scnr.next();
 		}
+		
+		System.out.println("Wins = " + wins + " Losses = " + losses);
 
 	}
 
